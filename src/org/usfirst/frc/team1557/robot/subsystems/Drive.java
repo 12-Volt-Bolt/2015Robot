8cbab0556ebdfa04a086a5048f4e6596e32d3094
Team1557 <team1557@gmail.com>
2015-01-13 17:41:53 -0500

    Initial commit

---
+++ src/org/usfirst/frc/team1557/robot/subsystems/Drive.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1557.robot.subsystems;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drive extends Subsystem {
+    
+   RobotDrive drive;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
