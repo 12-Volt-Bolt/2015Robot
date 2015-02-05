c2d08c992c0c00ace96b79fd0482adfbbb48765a
Team1557 <team1557@gmail.com>
2015-02-04 20:02:20 -0500

    MagnetoFun

--- src/org/usfirst/frc/team1557/robot/sensor/LSM303DLHC_Magneto.java
+++ src/org/usfirst/frc/team1557/robot/sensor/LSM303DLHC_Magneto.java
@@ -2,63 +2,76 @@
 //
 //import edu.wpi.first.wpilibj.I2C;
 //
-//public class LSM303DLHC_Accel {
+//public class LSM303DLHC_Magneto {
 //
-//	public enum Scale {
-//		g2, g4, g8, g16;
+//	public enum Speed {
+//		HZ075, HZ150, HZ300, HZ750, HZ1500, HZ3000, HZ7500, HZ22000;
 //	}
 //	
-//	private final int devAddress = 0b0011_001;
+//	
+//	private final int devAddress = 0b0011_110;
 //	private final int regCtrl1 = 0x20;
 //	private final int regCtrl4 = 0x23;
-//	private final int regOutXL = 0x28;
-//	private final int regOutXH = 0x29;
-//	private final int regOutYL = 0x2a;
-//	private final int regOutYH = 0x2b;
-//	private final int regOutZL = 0x2c;
-//	private final int regOutZH = 0x2d;
-//	private Scale currScale = Scale.g2;
-//	private I2C accel;
+//	private final int regOutXL = 0x04;
+//	private final int regOutXH = 0x03;
+//	private final int regOutYL = 0x08;
+//	private final int regOutYH = 0x07;
+//	private final int regOutZL = 0x06;
+//	private final int regOutZH = 0x05;
+//	private Speed currRate = Speed.HZ075;
+//	private I2C magneto;
 //
 //	// TODO allow setting of data rate
-//	public LSM303DLHC_Accel() {
+//	public LSM303DLHC_Magneto() {
 //		try {
-//			accel = new I2C(I2C.Port.kOnboard, devAddress);
+//			magneto = new I2C(I2C.Port.kOnboard, devAddress);
 //
 //			byte[] buf = new byte[1];
-//			accel.read(regCtrl1, 1, buf);
+//			magneto.read(regCtrl1, 1, buf);
 //			if ((buf[0] != 0b0000_0111) && (buf[0] != 0b0101_0111)) {
 //				throw new Exception(
-//						"Wrong device at LSM303DLHC Accelerometer address!");
+//						"Wrong device at LSM303DLHC Magnetometer address!");
 //			}
 //
-//			accel.write(regCtrl1, 0b0101_0111);
+//			magneto.write(regCtrl1, 0b0101_0111);
 //		} catch (Exception ex) {
 //			ex.printStackTrace();
-//			if (accel != null) {
-//				accel.free();
-//				accel = null;
+//			if (magneto != null) {
+//				magneto.free();
+//				magneto = null;
 //			}
 //		}
 //	}
 //
-//	public void setScale(Scale scale) {
-//		if (accel != null) {
-//			switch (scale) {
-//			case g2:
-//				accel.write(regCtrl4, 0b0000_0000);
+//	public void setScale(Speed rate) {
+//		if (magneto != null) {
+//			switch (rate) {
+//			case HZ075:
+//				magneto.write(regCtrl4, 0b0000_0000);
 //				break;
-//			case g4:
-//				accel.write(regCtrl4, 0b0001_0000);
+//			case HZ150:
+//				magneto.write(regCtrl4, 0b0000_0100);
 //				break;
-//			case g8:
-//				accel.write(regCtrl4, 0b0010_0000);
+//			case HZ300:
+//				magneto.write(regCtrl4, 0b0000_1000);
 //				break;
-//			case g16:
-//				accel.write(regCtrl4, 0b0011_0000);
+//			case HZ750:
+//				magneto.write(regCtrl4, 0b0000_1100);
+//				break;
+//			case HZ1500:
+//				magneto.write(regCtrl4, 0b0001_0000);
+//				break;
+//			case HZ3000:
+//				magneto.write(regCtrl4, 0b0001_0100);
+//				break;
+//			case HZ7500:
+//				magneto.write(regCtrl4, 0b0001_1000);
+//				break;
+//			case HZ22000:
+//				magneto.write(regCtrl4, 0b0001_1100);
 //				break;
 //			}
-//			currScale = scale;
+//			currRate = rate;
 //		}
 //	}
 //
@@ -76,30 +89,43 @@
 //
 //	public double readRate(int lowAddr, int highAddr) {
 //		double scaledVal = 0;
-//		if (accel != null) {
+//		if (magneto != null) {
 //			int val;
 //			byte[] buf = new byte[1];
 //
-//			accel.read(lowAddr, 1, buf);
+//			magneto.read(lowAddr, 1, buf);
 //			val = ((int) buf[0]) & 0b0000_0000_1111_1111;
 //
-//			accel.read(highAddr, 1, buf);
+//			magneto.read(highAddr, 1, buf);
 //			val |= ((int) buf[0]) << 8;
 //
-//			switch (currScale) {
-//			case g2:
+//			switch (currRate) {
+//			case HZ075:
 //				scaledVal = ((double) val) * 2 / 0b0111_1111_1111_1111;
 //				break;
-//			case g4:
+//			case HZ150:
 //				scaledVal = ((double) val) * 4 / 0b0111_1111_1111_1111;
 //				break;
-//			case g8:
+//			case HZ300:
 //				scaledVal = ((double) val) * 8 / 0b0111_1111_1111_1111;
 //				break;
-//			case g16:
+//			case HZ750:
+//				scaledVal = ((double) val) * 16 / 0b0111_1111_1111_1111;
+//				break;
+//			case HZ1500:
+//				scaledVal = ((double) val) * 2 / 0b0111_1111_1111_1111;
+//				break;
+//			case HZ3000:
+//				scaledVal = ((double) val) * 4 / 0b0111_1111_1111_1111;
+//				break;
+//			case HZ7500:
+//				scaledVal = ((double) val) * 8 / 0b0111_1111_1111_1111;
+//				break;
+//			case HZ22000:
 //				scaledVal = ((double) val) * 16 / 0b0111_1111_1111_1111;
 //				break;
 //			}
+//			
 //		}
 //
 //		return scaledVal;
@@ -107,7 +133,7 @@
 //
 //	// TODO may return incorrect values
 //	public boolean isConnected() {
-//		return accel != null;
+//		return magneto != null;
 //
 //	}
 //}
